077162939ce4c9330a425c79db7c5f7c8018ff48
Baker Moran <baker.moran@joinroot.com>
2020-08-14 11:37:09 -0400

    adds error handling

--- sample_size/api/sample_size.py
+++ sample_size/api/sample_size.py
@@ -101,27 +101,41 @@ def get_sample_size():
     """Get sample size estimate for parameters"""
 
     context = {}
+    code = 200
+    if flask.request.args.get('baseline_conversion_rate') is None:
+        code = 404
+        context['response_code'] = 404
+        context['message'] = 'baseline_conversion_rate is a required argument'
+    elif flask.request.args.get('expected_relative_lift') is None:
+        code = 404
+        context['response_code'] = 404
+        context['message'] = 'expected_relative_lift is a required argument'
+    else:
+        baseline_conversion_rate = flask.request.args.get(
+                                        'baseline_conversion_rate', type=float)
+        expected_relative_lift = flask.request.args.get(
+                                          'expected_relative_lift', type=float)
+        loss_tolerance = flask.request.args.get('loss_tolerance',
+                                                default=.02, type=float)
+        prior_alpha = flask.request.args.get('prior_alpha',
+                                             default=1, type=int)
+        prior_beta = flask.request.args.get('prior_beta', default=1, type=int)
 
-    baseline_conversion_rate = flask.request.args.get('baseline_conversion_rate', default=.3, type=float)
-    expected_relative_lift = flask.request.args.get('expected_relative_lift', default=.2, type=float)
-    loss_tolerance = flask.request.args.get('loss_tolerance', default=.02, type=float)
-    prior_alpha = flask.request.args.get('prior_alpha', default=1, type=int)
-    prior_beta = flask.request.args.get('prior_beta', default=1, type=int)
+        results = _sample_size_fixed_loss_tolerance(
+                             baseline_conversion_rate=baseline_conversion_rate,
+                             expected_relative_lift=expected_relative_lift,
+                             loss_tolerance=loss_tolerance,
+                             prior_alpha=prior_alpha,
+                             prior_beta=prior_beta)
 
-    results = _sample_size_fixed_loss_tolerance(baseline_conversion_rate=baseline_conversion_rate,
-                                      expected_relative_lift=expected_relative_lift,
-                                      loss_tolerance=loss_tolerance,
-                                      prior_alpha=prior_alpha,
-                                      prior_beta=prior_beta)
+        context['url'] = flask.request.path
+        inputs = {}
+        inputs['baseline_conversion_rate'] = baseline_conversion_rate
+        inputs['expected_relative_lift'] = expected_relative_lift
+        inputs['loss_tolerance'] = loss_tolerance
+        inputs['prior_alpha'] = prior_alpha
+        inputs['prior_beta'] = prior_beta
+        context['inputs'] = inputs
+        context['outputs'] = results
 
-    context['url'] = flask.request.path
-    inputs = {}
-    inputs['baseline_conversion_rate'] = baseline_conversion_rate
-    inputs['expected_relative_lift'] = expected_relative_lift
-    inputs['loss_tolerance'] = loss_tolerance
-    inputs['prior_alpha'] = prior_alpha
-    inputs['prior_beta'] = prior_beta
-    context['inputs'] = inputs
-    context['outputs'] = results
-
-    return flask.jsonify(**context)
+    return flask.jsonify(**context), code
--- sample_size/api/v1.py
+++ sample_size/api/v1.py
@@ -6,5 +6,11 @@ import sample_size
 @sample_size.app.route('/api/v1/', methods=["GET"])
 def get_v1():
     """Return list of services available."""
+    context = {}
+    context['available_services'] = {'sample_size': "/api/v1/sample_size"}
+    return flask.jsonify(**context)
 
-    return flask.jsonify({'sample_size': "/api/v1/sample_size"})
+
+@sample_size.app.errorhandler(404)
+def resource_not_found(e):
+    return flask.jsonify(error=str(e)), 404
